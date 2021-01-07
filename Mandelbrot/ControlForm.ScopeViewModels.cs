0a37ae196f237642aef1b1b23760687875686ded
René Vogt <rvogt@aki-gmbh.de>
2021-01-07 22:44:01 +0100

    removed obsolete type converter

--- Mandelbrot/ControlForm.ScopeViewModels.cs
+++ Mandelbrot/ControlForm.ScopeViewModels.cs
@@ -13,21 +13,17 @@ namespace Mandelbrot
             public double Imaginary { get; set; }
             public override string ToString() => new Complex(Real, Imaginary).ToString();
         }
-        sealed class ComplexTypeConverter : ExpandableObjectConverter
-        {
-
-        }
         sealed class ScopeViewModel
         {
             [DisplayName("Lower left corner")]
             [Category("Complex rectangle")]
             [DefaultValue(typeof(ComplexViewModel), "(-2, -2")]
-            [TypeConverter(typeof(ComplexTypeConverter))]
+            [TypeConverter(typeof(ExpandableObjectConverter))]
             public ComplexViewModel LowerLeft { get; } = new ComplexViewModel();
             [DisplayName("Upper right corner")]
             [Category("Complex rectangle")]
             [DefaultValue(typeof(ComplexViewModel), "(-2, -2")]
-            [TypeConverter(typeof(ComplexTypeConverter))]
+            [TypeConverter(typeof(ExpandableObjectConverter))]
             public ComplexViewModel UpperRight { get; } = new ComplexViewModel();
         }
     }
